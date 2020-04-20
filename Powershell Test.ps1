b3e352ecbd88ac3149f1f602d5d237662471a033
KaneToussaint <64018410+KaneToussaint@users.noreply.github.com>
2020-04-20 14:41:33 +0100

    Update Powershell Test.ps1

--- Powershell Test.ps1
+++ Powershell Test.ps1
@@ -1,5 +1,19 @@
-﻿Import-Csv -Path c:\users\ktoussaint\desktop\users.csv
-get-content C:\users\ktoussaint\desktop\Users.csv | convertfrom-csv | Measure-Object 
-get-content C:\users\ktoussaint\desktop\Users.csv | convertfrom-csv | Measure-Object "MailboxSizeGB" -Sum
-get-content C:\users\ktoussaint\desktop\Users.csv | convertfrom-csv | Compare-Object -property UserPrincipalName,EmailAddress
-get-content C:\users\ktoussaint\desktop\Users.csv | convertfrom-csv | group-Object -Property site | Select-Object name
+$Import = Import-Csv -Path c:\users\ktoussaint\desktop\users.csv
+
+$Import
+
+$question2 = $Import | Measure-Object
+
+$question2
+
+$question3 = $Import | Measure-Object "MailboxSizeGB" -Sum
+
+$question3
+
+$question4 = $Import |  Compare-Object -property UserPrincipalName,EmailAddress
+
+$question4
+
+$question5 = $Import | group-Object -Property site | Select-Object name
+
+$question5
