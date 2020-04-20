0f9c5d32be37cc8867dee118ea79f1fa31b8fe68
KaneToussaint <64018410+KaneToussaint@users.noreply.github.com>
2020-04-20 14:07:37 +0100

    Add files via upload

---
+++ Powershell Test.ps1
@@ -0,0 +1,5 @@
+﻿Import-Csv -Path c:\users\ktoussaint\desktop\users.csv
+get-content C:\users\ktoussaint\desktop\Users.csv | convertfrom-csv | Measure-Object 
+get-content C:\users\ktoussaint\desktop\Users.csv | convertfrom-csv | Measure-Object "MailboxSizeGB" -Sum
+get-content C:\users\ktoussaint\desktop\Users.csv | convertfrom-csv | Compare-Object -property UserPrincipalName,EmailAddress
+get-content C:\users\ktoussaint\desktop\Users.csv | convertfrom-csv | group-Object -Property site | Select-Object name
